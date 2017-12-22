70a8b63d760b0472d707a7e7f09fdc11d85088e9
Siraj Zaneer <szaneer@gmail.com>
2017-12-22 16:45:29 -0600

    Added ImagePicker to select image

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,21 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
 class PhotoMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    var currentImage: UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //one degree of latitude is approximately 111 kilometers (69 miles) at all times.
+        cameraButton.layer.cornerRadius = cameraButton.frame.height / 2.0
+        
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                              MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
     }
     
-
+    @IBAction func onCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
     
     // MARK: - Navigation
 
@@ -32,4 +44,16 @@ class PhotoMapViewController: UIViewController {
     }
     
 
+}
+
+extension PhotoMapViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            picker.dismiss(animated: true, completion: {
+                self.currentImage = image
+                self.performSegue(withIdentifier: "tagSegue", sender: nil)
+            })
+        }
+    }
 }
